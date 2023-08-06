753a6a96aec5c15ae566ec6f65c2e722aad96d88
pfernandez1990 <112211500+pfernandez1990@users.noreply.github.com>
2023-08-06 19:16:55 -0400

    Agregando inyeccines de dependencias y dbcontext

--- Program.cs
+++ Program.cs
@@ -1,11 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using WarehouseApi.Data;
+using WarehouseApi.Interfaces;
+using WarehouseApi.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+/* Agregando mapper */
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+/* Agregando los repositorios y las interfaces */
+builder.Services.AddScoped<IWarehouseRepository, WarehouseRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+/* Agregando el DbContext */
+builder.Services.AddDbContext<WarehouseContext>(Options => {
+    Options.UseSqlServer(builder.Configuration.GetConnectionString("WarehouseDB"));
+});
 
 var app = builder.Build();
 
